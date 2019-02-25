7919a40ddd0401e64508b785610af7470ebba595
Jared Wheeler <jared@jaredwheeler.com>
2019-02-25 11:53:44 -0600

    added initial XCode project

---
+++ BirdBits/BirdBits/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  BirdBits
+//
+//  Created by Jared Wheeler on 2/25/19.
+//  Copyright © 2019 Bird Audio Machines. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
